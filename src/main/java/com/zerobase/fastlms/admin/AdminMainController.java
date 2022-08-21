7a1088bc305cf330c0ac5d77171c2fd0711ee604
dohoonkim <dohoonkim023@gmail.com>
2022-08-21 20:52:06 +0900

    fastlms 관리자 회원 검색기능

--- src/main/java/com/zerobase/fastlms/admin/AdminMainController.java
+++ src/main/java/com/zerobase/fastlms/admin/AdminMainController.java
@@ -1,8 +1,11 @@
 package com.zerobase.fastlms.admin;
 
 
+import com.zerobase.fastlms.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class AdminMainController {
--- src/main/java/com/zerobase/fastlms/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/zerobase/fastlms/member/service/impl/MemberServiceImpl.java
@@ -1,5 +1,8 @@
 package com.zerobase.fastlms.member.service.impl;
 
+import com.zerobase.fastlms.admin.dto.MemberDto;
+import com.zerobase.fastlms.admin.mapper.MemberMapper;
+import com.zerobase.fastlms.admin.model.MemberParam;
 import com.zerobase.fastlms.components.MailComponents;
 import com.zerobase.fastlms.member.entity.Member;
 import com.zerobase.fastlms.member.exception.MemberNotEmailAuthException;
@@ -30,6 +33,9 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
     private final MailComponents mailComponents;
+
+    private final MemberMapper memberMapper;
+
 
     /**
      * 회원 가입
@@ -165,6 +171,16 @@ public class MemberServiceImpl implements MemberService {
         }
 
         return true;
+    }
+
+    @Override
+    public List<MemberDto> list(MemberParam parameter) {
+
+        List<MemberDto> list = memberMapper.selectList(parameter);
+
+        return list;
+
+        //return memberRepository.findAll();
     }
 
     @Override
